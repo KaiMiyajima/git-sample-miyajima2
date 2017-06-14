43802e060759349ec2d2ec98d944b7e4ebf0696c
kai.miyajima <kai.miyajima@rakus.co.jp>
2017-06-14 10:51:00 +0900

    github練習量リポジトリ.

---
+++ src/main/java/com/example/demo/GitSampleKai2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleKai2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleKai2Application.class, args);
+	}
+}
